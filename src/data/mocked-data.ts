e78b790c2af41d767461ce826c710ff0634a2025
jennliao <jennifer_liao@brown.edu>
2024-02-27 15:40:14 -0500

    chore: added additional dataset

--- src/data/mocked-data.ts
+++ src/data/mocked-data.ts
@@ -6,6 +6,14 @@ const mockedPresidentData = [
   ["Ronald", "Reagan", "Nancy Reagan", "1976"],
 ];
 
+const mockedPresidentNoHeader = [
+  ["first", "last", "spouse", "elected"],
+  ["Jinho", "Lee", "Thomas Doeppner", "2016"],
+  ["George", "Washington", "Martha Washington", "1789"],
+  ["Thomas", "Jefferson", "Ben Franklin", "2024"],
+  ["Ronald", "Reagan", "Nancy Reagan", "1976"],
+];
+
 const mockedDataResults = [["Jinho", "Lee", "Thomas Doeppner", "2016"]];
 
 export { mockedPresidentData, mockedDataResults };
